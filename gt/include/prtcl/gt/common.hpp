874b6e80944a47132d39b101be1fd60930134868
Daniel Edwards <git@danieledwards.de>
2020-05-15 15:37:53 +0200

    removed unused files and types

--- gt/include/prtcl/gt/common.hpp
+++ gt/include/prtcl/gt/common.hpp
@@ -1,19 +1,8 @@
 #pragma once
 
-//#include <prtcl/core/nd_dtype.hpp>
-//#include <prtcl/core/nd_index.hpp>
-//#include <prtcl/core/nd_shape.hpp>
-
 #include <prtcl/core/value_ptr.hpp>
 
 namespace prtcl::gt {
-
-//using ::prtcl::core::nd_dtype;
-//
-//using ::prtcl::core::make_nd_shape;
-//using ::prtcl::core::nd_shape;
-//
-//using ::prtcl::core::nd_index;
 
 using ::prtcl::core::value_ptr;
 
--- rt/include/prtcl/rt/common.hpp
+++ rt/include/prtcl/rt/common.hpp
@@ -2,19 +2,12 @@
 
 #include <prtcl/core/common.hpp>
 #include <prtcl/core/dtype.hpp>
-#include <prtcl/core/nd_index.hpp>
-#include <prtcl/core/nd_shape.hpp>
 #include <prtcl/core/ndtype.hpp>
 
 namespace prtcl::rt {
 
 using ::prtcl::core::dtype;
 using ::prtcl::core::ndtype;
-
-using ::prtcl::core::make_nd_shape;
-using ::prtcl::core::nd_shape;
-
-using ::prtcl::core::nd_index;
 
 using ::prtcl::core::ssize_t;
 
