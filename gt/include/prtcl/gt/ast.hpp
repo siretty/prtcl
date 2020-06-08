34faf4fc2fcfb57be4e5602f4d3f1c612d683fbc
Daniel Edwards <git@danieledwards.de>
2020-06-08 15:13:54 +0200

    removed old generator (boost::spirit::x3 based) (replaced by lua with lpeg)

--- gt/include/prtcl/gt/ast.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// declaration and definition of the ast types
-#include "bits/ast_define.hpp"
-
-// adapt the ast types as boost::fusion seqences for usage with
-// boost::spirit::x3
-#include "bits/ast_fusion.hpp"
-
--- gt/include/prtcl/gt/common.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <prtcl/core/value_ptr.hpp>
-
-namespace prtcl::gt {
-
-using ::prtcl::core::value_ptr;
-
-} // namespace prtcl::gt
