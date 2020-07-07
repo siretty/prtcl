3896b48aa4105acd49bba32d13235c02134c6023
Daniel Edwards <siretty@users.noreply.github.com>
2020-07-07 11:39:57 +0200

    skip already installed packages in setup.sh

--- share/building/setup.sh
+++ share/building/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S git lua clang cmake boost openmp eigen catch2 gtest lua-lpeg ninja htop
+sudo pacman --needed -S git lua clang cmake boost openmp eigen catch2 gtest lua-lpeg ninja htop
 
 git clone https://github.com/siretty/prtcl prtcl
 
