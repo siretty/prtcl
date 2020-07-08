7acb51597286fc90d413574a6872214bc1f5e178
Daniel Edwards <git@danieledwards.de>
2020-07-08 14:01:06 +0200

    removed previous implementation

--- rt/include/prtcl/rt/type_traits.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "type_traits/is_math_policy.hpp"
-#include "type_traits/is_model_policy.hpp"
-
-#include "type_traits/model_policy/policy_types.hpp"
-
-#include "type_traits/math_policy/ndtype.hpp"
--- rt/tests/prtcl/rt/grouped_uniform_grid.cpp
+++
@@ -1,5 +0,0 @@
-#include <catch2/catch.hpp>
-
-TEST_CASE("prtcl/rt/grouped_uniform_grid.hpp", "[prtcl][rt]") {
-  //
-}
