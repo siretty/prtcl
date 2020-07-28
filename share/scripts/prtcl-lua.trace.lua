a46c4d36fd375384ee7281efcca22153ce746bfb
Daniel Edwards <git@danieledwards.de>
2020-07-28 15:00:16 +0200

    added script to trace models

---
+++ share/scripts/prtcl-lua.trace.lua
@@ -0,0 +1,35 @@
+local prtcl = require 'prtcl'
+local rvec, rmat = prtcl.math.rvec, prtcl.math.rmat
+
+local model_path = args[3]
+local ppm_path = model_path .. '.ppm'
+
+local model = prtcl.data.model.load_native_binary(model_path)
+
+local camera = prtcl.geometry.pinhole_camera.new()
+camera.sensor_width, camera.sensor_height = 800, 600
+camera.focal_length = 1
+camera.origin = rvec.new { 0, 0, -1.3 }
+camera.principal = rvec.new { 0, 0, 1 }
+camera.up = rvec.new { 0, 1, 0 }
+
+local tracer = prtcl.util.sphere_tracer.new(camera)
+local image = tracer:trace(model)
+print('IMAGE', image.width, image.height)
+
+local ppm = io.open(ppm_path, 'wb')
+ppm:write("P6\n")
+ppm:write(tostring(image.width) .. "\n")
+ppm:write(tostring(image.height) .. "\n")
+ppm:write("255\n")
+for iy = 0, image.height - 1, 1 do
+  for ix = 0, image.width - 1, 1 do
+    local gray = math.floor(image:get_pixel(ix, iy) * 255)
+    ppm:write(string.char(gray, gray, gray))
+    --ppm:write(string.char(ix % 256))
+    --ppm:write(string.char(ix % 256))
+    --ppm:write(string.char(ix % 256))
+  end
+end
+io.close(ppm)
+
